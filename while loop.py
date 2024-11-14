0139375aee52d0c23dcf61f74d81d8c119cbbbf3
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-14 10:23:40 -0700

    Create while loop.py

---
+++ while loop.py
@@ -0,0 +1,40 @@
+# WHILE LOOPS
+
+print("Question 1:")
+# Q1: Write a while loop:
+# While num is smaller than 10, increase num by 1
+# and print it to the console
+# Do not touch this code:
+num = 2
+# Write your code below:
+
+print()
+print("Question 2:")
+# Q2: Write a while loop:
+# While x is smaller than 500, multiply x by 5
+# and print it to the console
+# Do not touch this code:
+x = 3
+# Write your code below:
+
+print()
+print("Question 3:")
+# Q3: Write a while loop:
+# While i is bigger than 5, subtract 10 from i
+# and print it to the console
+# Do not touch this code:
+i = 100
+# Write your code below:
+
+print()
+print("Question 4:")
+# Q4: Write a while loop:
+# While y is bigger than 1, divide y by 2
+# and print it to the console
+# Do not touch this code:
+y = 1000
+# Write your code below:
+
+print()
+print("Question 5:")
+#Q5. Write a Python program that will use a while loop to count from 10 down to, and including, 0. Then print the words ``Blast off!''.
